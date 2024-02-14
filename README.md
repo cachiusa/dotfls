4c149a6f016674ef3173fbfd70b6aba1ad3681db
cachiusa <nhat.dogpro@outlook.com>
2024-02-14 10:42:03 +0700

    new file:   README.md 	modified:   setup.sh

---
+++ README.md
@@ -0,0 +1,5 @@
+# .zshrc, based on Kali Linux default one
+## Setup
+```shellscript
+curl -LSs https://raw.githubusercontent.com/cachiusa/dotfls/main/.zshrc | bash -
+```
--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -x
+current_usr=$USER
 sudo apt-get update
-sudo apt-get install zsh zsh-syntax-highlighting
+sudo apt-get install zsh zsh-syntax-highlighting -y
 git clone --depth=1 https://github.com/romkatv/gitstatus.git ~/gitstatus
 curl -LSs https://raw.githubusercontent.com/cachiusa/dotfls/main/.zshrc > ~/.zshrc
-chsh -s /usr/bin/zsh
+sudo chsh -s /usr/bin/zsh $current_usr
